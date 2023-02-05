53d57c8f06900b1a9ba588d015c3dccaa113231a
alo40 <alejandronieto40@gmail.com>
2023-02-05 15:00:39 +0100

    add normal vector in 3d_plot_example

--- 3d_plot_example.py
+++ 3d_plot_example.py
@@ -6,7 +6,7 @@ Created on Sun Feb  5 11:20:17 2023
 """
 
 ## libraries
-# from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -30,13 +30,20 @@ Z = plane_general_form(X, Y, 10, 8, 3, -155)
 ## plot origin
 ax.scatter(0, 0, 0, c='red', marker='.', s=100)
 
-## plot plane 
-ax.plot_surface(X, Y, Z, 
-                edgecolor='royalblue', 
-                lw=0.5, 
-                rstride=8, 
-                cstride=8,
-                alpha=0.2)
+## plot normal vector
+x_n = [0,1]
+y_n = [0,1]
+z_n = [0,1]
+ax.plot(x_n, y_n, z_n)
+ax.scatter(x_n[1], y_n[1], z_n[1], c='red', marker='v', s=100)
+
+# ## plot plane 
+# ax.plot_surface(X, Y, Z, 
+#                 edgecolor='royalblue', 
+#                 lw=0.5, 
+#                 rstride=8, 
+#                 cstride=8,
+#                 alpha=0.2)
 
 ## plot config
 ax.set(xlabel='X', ylabel='Y', zlabel='Z')
