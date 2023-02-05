1f033af394854007e0a4fb19a44b8a495fae95b3
alo40 <alejandronieto40@gmail.com>
2023-02-05 12:09:58 +0100

    add origin to 3d_plot_example

--- 3d_plot_example.py
+++ 3d_plot_example.py
@@ -5,25 +5,28 @@ Created on Sun Feb  5 11:20:17 2023
 @author: fori
 """
 
-# libraries
-from mpl_toolkits.mplot3d import axes3d
+## libraries
+# from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 
-# figure config
+## figure config
 ax = plt.figure().add_subplot(projection='3d')
 
-# plane def from general form 
+## plane def from general form 
 def plane(x, y):
     return (10*x + 8*y - 155)/(-3)
 
-# data
+## data
 x = np.linspace(-6, 6, 100)
 y = np.linspace(-6, 6, 100)
 X, Y = np.meshgrid(x, y)
 Z = plane(X, Y)
 
-# plot 
+## plot origin
+ax.scatter(0, 0, 0, c='red', marker='.', s=100)
+
+## plot plane 
 ax.plot_surface(X, Y, Z, 
                 edgecolor='royalblue', 
                 lw=0.5, 
