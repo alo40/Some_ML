52314d1c015a3e5d316108294dd8ec2574ec2d50
alo40 <alejandronieto40@gmail.com>
2023-02-18 09:49:25 +0100

    update Perceptron_firstApproach.py: include comments and clean code

--- HO_0/Perceptron_firstApproach.py
+++ HO_0/Perceptron_firstApproach.py
@@ -8,41 +8,53 @@ Created on Sun Feb 12 13:21:25 2023
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line(x, a, b, c):
-    Y = (-a*x - c)/b
-    x_offset = (-c-b**2*c)/(a+b**2/a)
-    y_offset = -b*(-c-1/a*x_offset)
-    offset = np.array([x_offset, y_offset])
-    return Y, offset 
 
-x = np.array([3, -1])
-y = -1
+# function to calculate line normal to characteristic vector and its projection on that line
+def line(x1, a, b, c):
+    x2 = (-a*x1 - c)/b
+    x1_offset = (-c-b**2*c)/(a+b**2/a)
+    x2_offset = -b*(-c-1/a*x1_offset)
+    xx_offset = np.array([x1_offset, x2_offset])
+    return x2, xx_offset
 
+
+# declare figure
+fig, ax = plt.subplots()
+
+# training data
+x_training = np.array([-2, -2])  # training point
+y_training = -1  # label of training point, it can take values [-1, 1]
+ax.plot(x_training[0], x_training[1], marker="o")
+ax.text(x_training[0], x_training[1], "P1")
+
+# characteristic vector
 theta = np.array([0, 0])
 theta_0 = 0
 
+# perceptron algorithm
 for n in range(2):
-    value = y*(np.dot(x, theta) + theta_0)
+    value = y_training*(np.dot(x_training, theta) + theta_0)
     if value <= 0:
         print(str(n) + " True,  value = " + str(value) + ", theta = " + str(theta))
-        theta = theta + y*x
-        theta_0 = theta_0 + y
+        theta = theta + y_training*x_training
+        theta_0 = theta_0 + y_training
     else:
-        print(str(n) + " False, value = " + str(value) + ", theta = " + str(theta)) 
+        print(str(n) + " False, value = " + str(value) + ", theta = " + str(theta))
 
-X = np.linspace(-10, 10, 100)
-offset = np.zeros(2)
-Y, offset = line(X, theta[0], theta[1], theta_0)
+# call function to calculate line normal to characteristic vector and its projection on that line
+x1_line = np.linspace(-10, 10, 100)
+x2_line, x_offset = line(x1_line, theta[0], theta[1], theta_0)
 
-fig, ax = plt.subplots()
+# plot characteristic vector
+ax.plot([0 + x_offset[0], theta[0] + x_offset[0]], [0 + x_offset[1], theta[1] + x_offset[1]], color='red')
 
-ax.plot(x[0], x[1], marker="o")
-ax.text(x[0], x[1], "P1")
-ax.plot([0 + offset[0], theta[0] + offset[0]],[0 + offset[1], theta[1] + offset[1]])
-ax.plot(X, Y)
+# plot line normal to characteristic vector
+ax.plot(x1_line, x2_line, color='purple')
 
+# configure figure
 ax.set(xlabel='x_1', ylabel='x_2')
 lim = 4
 ax.set(xlim=(-lim, lim), ylim=(-lim, lim))
-ax.grid(linestyle = '--')
+ax.grid(linestyle='--')
 ax.set_aspect('equal', 'box')
+plt.show()
