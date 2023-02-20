5fa8f1936df7989b333471bb19d9a9eb2fca0ccf
alo40 <alejandronieto40@gmail.com>
2023-02-20 01:06:40 +0100

    add Perceptron_HO1.py

---
+++ HO_1/Perceptron_HO1.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# function to calculate line normal to characteristic vector and its projection on that line
+def normal_line(a, b, c, x_lim, y_lim):
+    if a == 0:
+        x1_line = np.linspace(-x_lim, x_lim, 100)
+        x2_line = np.zeros(x1_line.shape)
+    elif b == 0:
+        x2_line = np.linspace(-y_lim, y_lim, 100)
+        x1_line = np.zeros(x2_line.shape)
+    else:
+        x1_line = np.linspace(-x_lim, x_lim, 100)
+        x2_line = (-a*x1_line - c)/b
+    return x1_line, x2_line
+    # x1_projection = (-c-b**2*c)/(a+b**2/a)
+    # x2_projection = -b*(-c-1/a*x1_projection)
+    # x_projection = np.array([x1_projection, x2_projection])
+    # return = x_projection
+
+
+# declare figure
+fig, ax = plt.subplots()
+
+# configure figure
+lim_x = 12
+lim_y = 12
+ax.set(xlabel='x_1', ylabel='x_2')
+ax.set(xlim=(-lim_x, lim_x), ylim=(-lim_y, lim_y))
+ax.grid(linestyle='--')
+ax.set_aspect('equal', 'box')
+
+# training points
+x1 = np.array([-1., 1, -1])  # x-coordinate
+x2 = np.array([-1, 0, 10])  # y-coordinate
+y = np.array([1., -1, 1])  # labels
+
+# characteristic vector
+theta = np.array([0, 0])
+theta_0 = 0
+
+# plot points
+for i in range(y.size):
+    if y[i] > 0:
+        ax.scatter(x1[i], x2[i], c='b')
+    else:
+        ax.scatter(x1[i], x2[i], c='r')
+
+# perceptron
+# loop_order = [0]
+loop_order = [1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]
+for i in loop_order:
+    point_i = np.array([x1[i], x2[i]])
+    if y[i]*np.dot(theta, point_i) <= 0:
+        theta = theta + y[i]*point_i
+
+# line normal to characteristic vector
+x1_classifier, x2_classifier = normal_line(theta[0], theta[1], theta_0, lim_x, lim_y)
+ax.plot(x1_classifier, x2_classifier, c='m')
+
+# end code
+plt.show()
+
+# ------------------------------------------------------
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import matplotlib.cm as cm
+#
+# x = np.arange(10)
+# ys = [i+x+(i*x)**2 for i in range(10)]
+#
+# colors = cm.rainbow(np.linspace(0, 1, len(ys)))
+# for y, c in zip(ys, colors):
+#     plt.scatter(x, y, color=c)
+#
+# plt.show()
